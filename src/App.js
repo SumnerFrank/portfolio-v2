a5e623d9f4f3444d1c9d52b78fd0f4a54e68695e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-05-08 06:59:38 -0400

    initial build commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+
+function App() {
+const [page, setPage] = useState(<About/>)
+  return (
+    <div>
+      <Nav
+      setPage={setPage}
+      Contact={Contact}
+      About={About}
+      Projects={Projects}
+      Resume={Resume}
+      />
+      {page}
+      <Footer
+      setPage = {setPage}
+      Contact={Contact}
+      About={About}
+      />
+    </div>
+  );
+}
+
+export default App;
